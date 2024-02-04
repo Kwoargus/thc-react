f19e28b799fd594bb7a9e73bb226a63567cc6cc8
Dmitrii Piskunov <dmitrii.a.piskunov@gmail.com>
2024-02-04 10:08:09 +0300

    перемещен стилевой компонент CenterDivWrapper на один каталог вверх (в src/page/style.ts), изменены пути к нему в импортах существующих файлов

--- src/page/BackendFactors/style.ts
+++ src/page/BackendFactors/style.ts
@@ -3,10 +3,3 @@ import styled from "styled-components";
 export const BackendFactors = styled.div`
   display: flex;
 `;
-export const CenterDivWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  height: 100%;
-  padding: 20px;
-`;
